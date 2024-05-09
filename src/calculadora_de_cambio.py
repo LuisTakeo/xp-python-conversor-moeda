1355738c4cb1b060cc49b0e171d1fc2e6e8b1b3b
LuisTakeo <luistakeo@hotmail.com>
2024-05-09 12:47:35 -0300

    feat: github actions configurado

--- src/calculadora_de_cambio.py
+++ src/calculadora_de_cambio.py
@@ -1,2 +1,8 @@
+import math
+
 def converter(valor, taxa):
-    return 0
+    if (valor <= 0):
+        return 0
+    iof = (100 - 1.1) / 100
+    valor_cambio = round(valor / taxa * iof, 2)
+    return valor_cambio
--- tests/testes_da_calculadora_de_cambio.py
+++ tests/testes_da_calculadora_de_cambio.py
@@ -2,13 +2,51 @@ from unittest import TestCase
 import src.calculadora_de_cambio as calc
 
 class TestesDaCalculadoraDeCambio(TestCase):
-    
+
     def teste_conversao_cambio_valor_0_taxa_5_03_retorna_0(self):
         # Arrange
         valor = 0
         taxa = 3.37
         resultado_esperado = 0
-        
+
+        # Act
+        resultado_obtido = calc.converter(valor, taxa)
+
+        # Assert
+        self.assertEqual(resultado_obtido, resultado_esperado)
+
+    def teste_conversao_cambio_valor_1_taxa_5_17_retorna_0_19(self):
+        # Arrange
+        valor = 1
+        taxa = 5.17
+        iof = (100 - 1.1) / 100
+        resultado_esperado = round(valor / taxa * iof, 2)
+
+        # Act
+        resultado_obtido = calc.converter(valor, taxa)
+
+        # Assert
+        self.assertEqual(resultado_obtido, resultado_esperado)
+
+    def teste_conversao_cambio_valor_negativo_retorna_0(self):
+        # Arrange
+        valor = -1
+        taxa = 5.17
+        resultado_esperado = 0
+
+        # Act
+        resultado_obtido = calc.converter(valor, taxa)
+
+        # Assert
+        self.assertEqual(resultado_obtido, resultado_esperado)
+
+    def teste_conversao_cambio_valor_1000_taxa_5_56_retorna_179_85(self):
+        # Arrange
+        valor = 1000
+        taxa = 5.56
+        iof = (100 - 1.1) / 100
+        resultado_esperado = round(valor / taxa * iof, 2) - 1
+
         # Act
         resultado_obtido = calc.converter(valor, taxa)
 
